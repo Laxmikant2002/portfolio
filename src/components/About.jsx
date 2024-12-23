04b083eb9e37710d58aed1f56e9866f64fefec28
Laxmikant Shinde <98510838+Laxmikant2002@users.noreply.github.com>
2024-12-23 15:13:53 +0530

    Add files via upload

---
+++ src/components/About.jsx
@@ -0,0 +1,68 @@
+import React from "react";
+import Tilt from "react-tilt";
+import { motion } from "framer-motion";
+
+import { styles } from "../styles";
+import { services } from "../constants";
+import { SectionWrapper } from "../hoc";
+import { fadeIn, textVariant } from "../utils/motion";
+
+const ServiceCard = ({ index, title, icon }) => (
+  <Tilt className='xs:w-[250px] w-full'>
+    <motion.div
+      variants={fadeIn("right", "spring", index * 0.5, 0.75)}
+      className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
+    >
+      <div
+        options={{
+          max: 45,
+          scale: 1,
+          speed: 450,
+        }}
+        className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'
+      >
+        <img
+          src={icon}
+          alt='web-development'
+          className='w-16 h-16 object-contain'
+        />
+
+        <h3 className='text-white text-[20px] font-bold text-center'>
+          {title}
+        </h3>
+      </div>
+    </motion.div>
+  </Tilt>
+);
+
+const About = () => {
+  return (
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
+      </motion.div>
+
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
+      >
+        Hello! I am Laxmikant Shinde, a passionate and dedicated B.Tech student specializing in Electronics and Telecommunication Engineering at YCCE, Nagpur. With a strong foundation in programming and web development, I have honed my skills through academic projects, internships, and personal endeavors.
+
+        I specialize in full-stack development, with proficiency in Python, JavaScript, Django, React.js, and Node.js. My experience includes building scalable web applications, integrating APIs, and exploring emerging technologies like cloud computing and AI.
+
+        As a fresher, I am eager to contribute to dynamic teams, tackle challenging problems, and continuously grow in a professional environment. I believe in delivering high-quality solutions that make an impact while learning and evolving as a developer.
+
+        I am actively seeking opportunities to bring my technical skills, creativity, and enthusiasm to innovative projects and teams. Let’s build something amazing together!
+      </motion.p>
+
+      <div className='mt-20 flex flex-wrap gap-10'>
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default SectionWrapper(About, "about");
